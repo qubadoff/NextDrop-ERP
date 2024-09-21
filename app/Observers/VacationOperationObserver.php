9d99cb2a20a993f26fef0ad1b6b4b8fc2e7d8fd9
qubadoff <admin@burncode.org>
2024-09-21 22:53:21 +0400

    I

---
+++ app/Observers/VacationOperationObserver.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\VacationDay;
+use Carbon\Carbon;
+
+class VacationOperationObserver
+{
+    /**
+     * Handle the VacationDay "created" event.
+     */
+    public function created(VacationDay $vacationDay): void
+    {
+        $startDate = Carbon::parse($vacationDay->vacation_start_date);
+        $endDate = Carbon::parse($vacationDay->vacation_end_date);
+
+        $totalDays = $startDate->diffInDays($endDate) + 1;
+
+        $vacationDay->vacation_all_days_count -= $totalDays;
+
+        $vacationDay->save();
+    }
+
+    /**
+     * Handle the VacationDay "updated" event.
+     */
+    public function updated(VacationDay $vacationDay): void
+    {
+        //
+    }
+
+    /**
+     * Handle the VacationDay "deleted" event.
+     */
+    public function deleted(VacationDay $vacationDay): void
+    {
+        //
+    }
+
+    /**
+     * Handle the VacationDay "restored" event.
+     */
+    public function restored(VacationDay $vacationDay): void
+    {
+        //
+    }
+
+    /**
+     * Handle the VacationDay "force deleted" event.
+     */
+    public function forceDeleted(VacationDay $vacationDay): void
+    {
+        //
+    }
+}
