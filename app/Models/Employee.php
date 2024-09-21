6eb82852ea939fe0755140db52becc3aedc14166
qubadoff <admin@burncode.org>
2024-09-21 22:16:22 +0400

    I

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Employee\EmployeePenalTypeEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -17,6 +18,7 @@ class Employee extends Model
 
     protected $casts = [
         'docs' => 'array',
+        'penal_type' => EmployeePenalTypeEnum::class,
     ];
 
     public function education(): HasMany
