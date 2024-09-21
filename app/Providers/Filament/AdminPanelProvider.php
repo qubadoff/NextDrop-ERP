1267a39ceb5963bf78d129f935556337a6d4db13
qubadoff <admin@burncode.org>
2024-09-21 21:48:06 +0400

    I

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -34,6 +34,7 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Blue,
             ])
             ->maxContentWidth(MaxWidth::Full)
+            ->sidebarWidth(100)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
