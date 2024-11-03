fb73433478409767ed07c86a51d6f70a3abca571
Siyasat Gubadov <virus@Siyasats-Laptop.local>
2024-11-03 19:25:02 +0400

    I

--- app/Filament/Resources/BranchResource/Pages/ViewBranch.php
+++ app/Filament/Resources/BranchResource/Pages/ViewBranch.php
@@ -24,7 +24,7 @@ class ViewBranch extends ViewRecord
                         TextEntry::make('location')->label('Location'),
                         TextEntry::make('field_of_action')->label('Field of action'),
                         TextEntry::make('employee_count')->label('Employee count'),
-                        ImageEntry::make('qr_code_path')->label('Qr kod')->width(300)->height(300),
+                        ImageEntry::make('qr_code_path')->label('Qr kod')->width(100)->height(100),
                     ])->columns(5),
             ]);
     }
