133eb0af387ad030b7c8588fe637aaddaf5ab586
Siyasat Gubadov <virus@Siyasats-Laptop.local>
2024-11-04 14:07:58 +0400

    Improvements

--- app/Http/Controllers/api/v1/Auth/AuthController.php
+++ app/Http/Controllers/api/v1/Auth/AuthController.php
@@ -13,7 +13,7 @@ class AuthController extends Controller
     public function login(Request $request): JsonResponse
     {
         $request->validate([
-            'email' => 'required|email|exists:employees,email',
+            'email' => 'required|email',
             'password' => 'required',
         ]);
 
