1cf5ac21bf76095dce8c4c16ca7fcf2da00a5024
Siyasat Gubadov <virus@Siyasats-Laptop.local>
2024-11-04 19:22:59 +0400

    Improvements

--- app/Http/Controllers/api/v1/Employee/EmployeeController.php
+++ app/Http/Controllers/api/v1/Employee/EmployeeController.php
@@ -81,21 +81,58 @@ class EmployeeController extends Controller
      */
     private function calculateDistance($lat1, $lon1, $lat2, $lon2): float
     {
-        $earthRadius = 6371000; // Radius of Earth in meters
+        $a = 6378137.0; // Semi-major axis of the Earth (meters)
+        $f = 1 / 298.257223563; // Flattening
+        $b = 6356752.314245; // Semi-minor axis
 
-        // Convert degrees to radians
         $lat1 = deg2rad($lat1);
-        $lon1 = deg2rad($lon1);
         $lat2 = deg2rad($lat2);
+        $lon1 = deg2rad($lon1);
         $lon2 = deg2rad($lon2);
 
-        // Differences in coordinates
-        $dLat = $lat2 - $lat1;
-        $dLon = $lon2 - $lon1;
+        $L = $lon2 - $lon1;
+        $U1 = atan((1 - $f) * tan($lat1));
+        $U2 = atan((1 - $f) * tan($lat2));
+        $sinU1 = sin($U1);
+        $cosU1 = cos($U1);
+        $sinU2 = sin($U2);
+        $cosU2 = cos($U2);
 
-        // Calculate distance in meters
-        return sqrt(pow($dLat * $earthRadius, 2) + pow(cos(($lat1 + $lat2) / 2) * $dLon * $earthRadius, 2));
+        $lambda = $L;
+        $lambdaP = 2 * M_PI;
+        $iterLimit = 100;
+        while (abs($lambda - $lambdaP) > 1e-12 && --$iterLimit > 0) {
+            $sinLambda = sin($lambda);
+            $cosLambda = cos($lambda);
+            $sinSigma = sqrt(($cosU2 * $sinLambda) * ($cosU2 * $sinLambda) +
+                ($cosU1 * $sinU2 - $sinU1 * $cosU2 * $cosLambda) * ($cosU1 * $sinU2 - $sinU1 * $cosU2 * $cosLambda));
+            if ($sinSigma == 0) {
+                return 0; // Co-incident points
+            }
+            $cosSigma = $sinU1 * $sinU2 + $cosU1 * $cosU2 * $cosLambda;
+            $sigma = atan2($sinSigma, $cosSigma);
+            $sinAlpha = $cosU1 * $cosU2 * $sinLambda / $sinSigma;
+            $cos2Alpha = 1 - $sinAlpha * $sinAlpha;
+            $cos2SigmaM = $cosSigma - 2 * $sinU1 * $sinU2 / $cos2Alpha;
+            $C = $f / 16 * $cos2Alpha * (4 + $f * (4 - 3 * $cos2Alpha));
+            $lambdaP = $lambda;
+            $lambda = $L + (1 - C) * $f * $sinAlpha *
+                ($sigma + C * $sinSigma * ($cos2SigmaM + C * $cosSigma * (-1 + 2 * $cos2SigmaM * $cos2SigmaM)));
+        }
+
+        if ($iterLimit == 0) {
+            return 0; // Formula failed to converge
+        }
+
+        $uSquared = $cos2Alpha * ($a * $a - $b * $b) / ($b * $b);
+        $A = 1 + $uSquared / 16384 * (4096 + $uSquared * (-768 + $uSquared * (320 - 175 * $uSquared)));
+        $B = $uSquared / 1024 * (256 + $uSquared * (-128 + $uSquared * (74 - 47 * $uSquared)));
+        $deltaSigma = $B * $sinSigma * ($cos2SigmaM + $B / 4 * ($cosSigma * (-1 + 2 * $cos2SigmaM * $cos2SigmaM) -
+                    $B / 6 * $cos2SigmaM * (-3 + 4 * $sinSigma * $sinSigma) * (-3 + 4 * $cos2SigmaM * $cos2SigmaM)));
+
+        return $b * $A * ($sigma - $deltaSigma); // Distance in meters
     }
+
 
 
 }
