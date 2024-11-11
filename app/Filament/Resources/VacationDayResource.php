56886993c50c0d683f0b066a21c1f4f51fd43843
Siyasat Gubadov <virus@Siyasats-Laptop.local>
2024-11-11 22:25:23 +0400

    C

--- app/Filament/Resources/VacationDayResource.php
+++ app/Filament/Resources/VacationDayResource.php
@@ -29,7 +29,7 @@ class VacationDayResource extends Resource
 
     protected static ?int $navigationSort = 10;
 
-    protected static ?string $navigationIcon = 'heroicon-o-clipboard-list';
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard';
 
     public static function form(Form $form): Form
     {
