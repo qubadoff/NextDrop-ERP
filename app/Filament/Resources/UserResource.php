ab82d496f84eab3f91ddf629d159355cafdabfd3
Siyasat Gubadov <virus@Siyasats-Laptop.local>
2024-11-11 22:16:30 +0400

    C

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -24,7 +24,7 @@ class UserResource extends Resource
 
     protected static ?string $label = 'Admin';
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-user';
+    protected static ?string $navigationIcon = 'heroicon-o-user';
 
     public static function form(Form $form): Form
     {
