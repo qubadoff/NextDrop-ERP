806ab5d4beed9b98ab4a36f24e055616d51e0381
Siyasat Gubadov <virus@Siyasats-Laptop.local>
2024-11-13 20:03:52 +0400

    C

--- app/Filament/Resources/EmployeeAttendanceResource.php
+++ app/Filament/Resources/EmployeeAttendanceResource.php
@@ -39,8 +39,8 @@ class EmployeeAttendanceResource extends Resource
                 Tables\Columns\TextColumn::make('employee.surname')->label('Soyad')->searchable(),
                 Tables\Columns\TextColumn::make('branch.name')->label('Filial')->searchable(),
                 Tables\Columns\TextColumn::make('employee.position.name')->label('Vəzifə')->searchable(),
-                Tables\Columns\TextColumn::make('employee_in')->label('Giriş vaxtı')->searchable()->time(),
-                Tables\Columns\TextColumn::make('employee_out')->label('Çıxış vaxtı')->searchable()->time(),
+                Tables\Columns\TextColumn::make('employee_in')->label('Giriş vaxtı')->searchable()->time('H:i'),
+                Tables\Columns\TextColumn::make('employee_out')->label('Çıxış vaxtı')->searchable()->time('H:i'),
                 Tables\Columns\TextColumn::make('duration')
                     ->label('İşdə olduğu müddət')
                     ->sortable()
