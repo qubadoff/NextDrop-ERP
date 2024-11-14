8d8441094ee600f6c05a7faf91b6c1948f326dab
Siyasat Gubadov <virus@Siyasats-Laptop.local>
2024-11-14 12:50:24 +0400

    C

--- app/Http/Controllers/api/v1/Employee/EmployeeController.php
+++ app/Http/Controllers/api/v1/Employee/EmployeeController.php
@@ -25,7 +25,7 @@ class EmployeeController extends Controller
 
         $attendancePaginated = EmployeeAttendance::where('employee_id', $employeeId)
             ->orderBy('created_at', 'desc')
-            ->paginate(20);
+            ->paginate(15);
 
         $attendanceData = collect($attendancePaginated->items())->groupBy(function ($attendance) {
             return $attendance->created_at->format('Y-m-d');
