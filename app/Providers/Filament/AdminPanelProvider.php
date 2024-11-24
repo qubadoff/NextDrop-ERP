4ba2b4acb652046e6df08510d5170421ca09da08
Siyasat Gubadov <virus@Siyasats-Laptop.local>
2024-11-24 22:34:03 +0400

    New

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -43,7 +43,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
+                //Widgets\AccountWidget::class,
                 PenalChart::class,
                 EmployeeAwardResource::class,
                 //Widgets\FilamentInfoWidget::class,
