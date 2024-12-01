a2a4cc25e2558664ea19e5eeb15c05f6cd5c6110
qubadoff <admin@burncode.org>
2024-12-01 19:25:09 +0400

    new

--- app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
+++ app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
@@ -8,4 +8,10 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateVacationDay extends CreateRecord
 {
     protected static string $resource = VacationDayResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+
 }
