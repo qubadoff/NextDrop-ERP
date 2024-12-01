416d2b3551c21b34260e0b148aa0734053527bbe
qubadoff <admin@burncode.org>
2024-12-01 19:02:24 +0400

    new

--- app/Models/VacationDay.php
+++ app/Models/VacationDay.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use App\Vacation\VacationPayTypeEnum;
 use App\Vacation\VacationStatusEnum;
 use App\Vacation\VacationTypeEnum;
+use Filament\Notifications\Notification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\DB;
 
 class VacationDay extends Model
 {
@@ -22,6 +24,33 @@ class VacationDay extends Model
         'vacation_type' => VacationTypeEnum::class,
         'status' => VacationStatusEnum::class,
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($vacationDay) {
+            $employeeId = $vacationDay->employee_id;
+
+            $dayLimit = DB::table('employee_vacation_day_options')
+                ->where('employee_id', $employeeId)
+                ->value('day_count');
+
+            $totalVacationDays = DB::table('vacation_days')
+                ->where('employee_id', $employeeId)
+                ->sum('vacation_day_count');
+
+            if ($totalVacationDays + $vacationDay->vacation_day_count > $dayLimit) {
+                Notification::make()
+                    ->title('Əməliyyat icra olunmadı !')
+                    ->danger()
+                    ->body('İşçinin məzuniyyət günlərinin sayı ' . $dayLimit . ' gündən artıq olmamalıdır !')
+                    ->send();
+
+                return false;
+            }
+        });
+    }
 
     public function employee(): BelongsTo
     {
--- app/Observers/VacationOperationObserver.php
+++ app/Observers/VacationOperationObserver.php
@@ -11,64 +11,15 @@ class VacationOperationObserver
 {
     /**
      * Handle the VacationDay "created" event.
-     * @throws \Exception
+     * @throws Exception
      */
     public function created(VacationDay $vacationDay): void
     {
-        DB::beginTransaction();
-
-        try {
-            $employeeId = $vacationDay->employee_id;
-
-            $dayLimit = DB::table('employee_vacation_day_options')
-                ->where('employee_id', $employeeId)
-                ->value('day_count');
-
-            $totalVacationDays = DB::table('vacation_days')
-                ->where('employee_id', $employeeId)
-                ->sum('vacation_day_count');
-
-            if ($totalVacationDays > $dayLimit) {
-                Notification::make()
-                    ->title('Əməliyyat icra olunmadı !')
-                    ->danger()
-                    ->body('İşçinin məzuniyyət günlərinin sayı ' . $dayLimit . ' gündən artıq olmamalıdır !')
-                    ->send();
-
-                DB::rollBack();
-                return;
-            }
-
-            DB::commit();
-        } catch (Exception $e) {
-            DB::rollBack();
-            throw $e;
-        }
+        //
     }
     public function updated(VacationDay $vacationDay): void
     {
-        $employeeId = $vacationDay->employee_id;
-
-        $dayLimit = DB::table('employee_vacation_day_options')
-            ->where('employee_id', $employeeId)
-            ->value('day_count');
-
-        $totalVacationDays = DB::table('vacation_days')
-            ->where('employee_id', $employeeId)
-            ->where('id', '!=', $vacationDay->id) // Güncellenen satır hariç
-            ->sum('vacation_day_count');
-
-        $newTotal = $totalVacationDays + $vacationDay->vacation_day_count;
-
-        if ($newTotal > $dayLimit) {
-            Notification::make()
-                ->title('Əməliyyat icra olunmadı !')
-                ->danger()
-                ->body('İşçinin məzuniyyət günlərinin sayı ' . $dayLimit - $totalVacationDays . ' - gündən artıq olmamalıdır !')
-                ->send();
-
-            DB::rollBack();
-        }
+        //
     }
 
 
