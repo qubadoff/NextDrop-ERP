b6a1f4c6feb7bb0aa2c324d1bc8e598307df3e25
qubadoff <admin@burncode.org>
2024-12-01 18:41:26 +0400

    new

--- app/Observers/VacationOperationObserver.php
+++ app/Observers/VacationOperationObserver.php
@@ -15,26 +15,7 @@ class VacationOperationObserver
      */
     public function created(VacationDay $vacationDay): void
     {
-        if ($vacationDay->status === VacationStatusEnum::APPROVED) {
-
-            $startDate = Carbon::parse($vacationDay->vacation_start_date);
-            $endDate = Carbon::parse($vacationDay->vacation_end_date);
-
-            // Calculate total days
-            $totalDays = $startDate->diffInDays($endDate) + 1;
-
-            // Check if the new vacation_all_days_count would be less than zero
-            $newVacationAllDaysCount = $vacationDay->vacation_all_days_count - $totalDays;
-
-            if ($newVacationAllDaysCount < 0) {
-                // Trigger an alert (this could be a flash message, exception, or log)
-                throw new \Exception('Vacation days cannot be negative!');
-            }
-
-            // Otherwise, proceed with updating and saving
-            $vacationDay->vacation_all_days_count = $newVacationAllDaysCount;
-            $vacationDay->save();
-        }
+        //
     }
 
     /**
@@ -42,21 +23,7 @@ class VacationOperationObserver
      */
     public function updated(VacationDay $vacationDay): void
     {
-        if ($vacationDay->status === VacationStatusEnum::APPROVED) {
-
-            // Calculate total vacation days between start and end dates
-            $startDate = Carbon::parse($vacationDay->vacation_start_date);
-            $endDate = Carbon::parse($vacationDay->vacation_end_date);
-            $totalDays = $startDate->diffInDays($endDate) + 1;
-
-            // Disable event dispatching temporarily to avoid recursion
-            DB::transaction(function () use ($vacationDay, $totalDays) {
-                $vacationDay->vacation_all_days_count -= $totalDays;
-
-                // Save the model without triggering events
-                $vacationDay->saveQuietly();
-            });
-        }
+        //
     }
 
     /**
