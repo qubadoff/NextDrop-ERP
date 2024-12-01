66a93403c2d7d952cce051f831812bdfe4919efe
qubadoff <admin@burncode.org>
2024-12-01 19:08:50 +0400

    new

--- app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
+++ app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
@@ -3,9 +3,9 @@
 namespace App\Filament\Resources\VacationDayResource\Pages;
 
 use App\Filament\Resources\VacationDayResource;
-use Filament\Actions;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
+use Filament\Support\Exceptions\Halt;
 use Illuminate\Support\Facades\DB;
 
 class CreateVacationDay extends CreateRecord
@@ -13,29 +13,28 @@ class CreateVacationDay extends CreateRecord
     protected static string $resource = VacationDayResource::class;
 
 
+    /**
+     * @throws Halt
+     */
     protected function mutateFormDataBeforeCreate(array $data): array
     {
         $employeeId = $data['employee_id'];
 
-        // Employee'nin vacation day limitini al
         $dayLimit = DB::table('employee_vacation_day_options')
             ->where('employee_id', $employeeId)
             ->value('day_count');
 
-        // Mevcut alınan vacation days toplamını hesapla
         $totalVacationDays = DB::table('vacation_days')
             ->where('employee_id', $employeeId)
             ->sum('vacation_day_count');
 
-        // Yeni tatil günüyle toplamı kontrol et
         if ($totalVacationDays + $data['vacation_day_count'] > $dayLimit) {
             Notification::make()
                 ->title('Əməliyyat icra olunmadı !')
                 ->danger()
-                ->body('İşçinin məzuniyyət günlərinin sayı ' . $dayLimit . ' gündən artıq olmamalıdır !')
+                ->body('İşçinin məzuniyyət günlərinin sayı ' . $dayLimit - $totalVacationDays . ' - gündən artıq olmamalıdır !')
                 ->send();
 
-            // İşlemi durdur
             $this->halt();
         }
 
