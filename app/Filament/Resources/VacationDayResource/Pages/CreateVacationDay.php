28027b97e14568ce3c29e8ea91e2eba83605bade
qubadoff <admin@burncode.org>
2024-12-01 19:14:52 +0400

    new

--- app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
+++ app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
@@ -20,7 +20,7 @@ class CreateVacationDay extends CreateRecord
     {
         $employeeId = $data['employee_id'];
 
-        dd($employeeId);
+        dd($data);
 
         $dayLimit = DB::table('employee_vacation_day_options')
             ->where('employee_id', $employeeId)
