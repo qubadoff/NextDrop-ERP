398187b286373316c426871a0bed372b87cd1ee8
qubadoff <admin@burncode.org>
2024-12-01 19:20:53 +0400

    new

--- app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
+++ app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
@@ -11,37 +11,4 @@ use Illuminate\Support\Facades\DB;
 class CreateVacationDay extends CreateRecord
 {
     protected static string $resource = VacationDayResource::class;
-
-
-    /**
-     * @throws Halt
-     */
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        $employeeId = $data['employee_id'];
-
-        dd($employeeId);
-
-        $dayLimit = DB::table('employee_vacation_day_options')
-            ->where('employee_id', $employeeId)
-            ->value('day_count');
-
-        $totalVacationDays = DB::table('vacation_days')
-            ->where('employee_id', $employeeId)
-            ->sum('vacation_day_count') ?? 0;
-
-        dd($dayLimit, $totalVacationDays);
-
-        if ($totalVacationDays + $data['vacation_day_count'] > $dayLimit) {
-            Notification::make()
-                ->title('Əməliyyat icra olunmadı !')
-                ->danger()
-                ->body('İşçinin məzuniyyət günlərinin sayı ' . $dayLimit - $totalVacationDays . ' - gündən artıq olmamalıdır !')
-                ->send();
-
-            $this->halt();
-        }
-
-        return $data;
-    }
 }
