e1f71c4b5e18805932495f7702184d6dc1d1333c
qubadoff <admin@burncode.org>
2024-12-01 19:27:12 +0400

    new

--- app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
+++ app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
@@ -4,14 +4,22 @@ namespace App\Filament\Resources\VacationDayResource\Pages;
 
 use App\Filament\Resources\VacationDayResource;
 use Filament\Resources\Pages\CreateRecord;
+use JetBrains\PhpStorm\NoReturn;
 
 class CreateVacationDay extends CreateRecord
 {
     protected static string $resource = VacationDayResource::class;
 
-    protected function getRedirectUrl(): string
+//    protected function getRedirectUrl(): string
+//    {
+//        return $this->getResource()::getUrl('index');
+//    }
+
+    #[NoReturn]
+    protected function mutateFormDataBeforeCreate(array $data): array
     {
-        return $this->getResource()::getUrl('index');
+        dd($data);
     }
+
 
 }
