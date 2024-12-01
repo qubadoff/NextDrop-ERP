a4e9aff514a19a5ba0afbada9b3dff60c9aa576d
qubadoff <admin@burncode.org>
2024-12-01 19:37:46 +0400

    new

--- app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
+++ app/Filament/Resources/VacationDayResource/Pages/CreateVacationDay.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\VacationDayResource\Pages;
 
 use App\Filament\Resources\VacationDayResource;
+use Carbon\Carbon;
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 use Filament\Support\Exceptions\Halt;
@@ -19,14 +20,33 @@ class CreateVacationDay extends CreateRecord
     {
         $employeeId = $data['employee_id'];
 
+        // Vacation day limitini al
         $dayLimit = DB::table('employee_vacation_day_options')
             ->where('employee_id', $employeeId)
             ->value('day_count');
 
+        // Mevcut toplam vacation days hesapla
         $totalVacationDays = DB::table('vacation_days')
             ->where('employee_id', $employeeId)
             ->sum('vacation_day_count');
 
+        // Tarihler arası farkı hesapla
+        $startDate = Carbon::parse($data['vacation_start_date']);
+        $endDate = Carbon::parse($data['vacation_end_date']);
+        $calculatedDays = $startDate->diffInDays($endDate) + 1; // Başlama ve bitiş günü dahil
+
+        // Vacation day count ile hesaplanan gün sayısını karşılaştır
+        if ($data['vacation_day_count'] != $calculatedDays) {
+            Notification::make()
+                ->title('Əməliyyat icra olunmadı!')
+                ->danger()
+                ->body('Məzuniyyət günlərinin sayı seçilən tarixlərə uyğun deyil. Hesaplanan gün sayı: ' . $calculatedDays)
+                ->send();
+
+            $this->halt();
+        }
+
+        // Vacation day limit kontrolü
         if ($totalVacationDays + $data['vacation_day_count'] > $dayLimit) {
             Notification::make()
                 ->title('Əməliyyat icra olunmadı!')
@@ -39,6 +59,7 @@ class CreateVacationDay extends CreateRecord
 
         return $data;
     }
+
 
 
 }
