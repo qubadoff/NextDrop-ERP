672d832adff22adf24b6715712d497348bd35991
qubadoff <admin@burncode.org>
2024-12-01 20:54:36 +0400

    new

--- app/Observers/EmployeeAttendanceObserver.php
+++ app/Observers/EmployeeAttendanceObserver.php
@@ -4,25 +4,37 @@ namespace App\Observers;
 
 use App\Models\EmployeeAttendance;
 use Carbon\Carbon;
+use Exception;
 
 class EmployeeAttendanceObserver
 {
     /**
      * Handle the EmployeeAttendance "created" event.
+     * @throws \Exception
      */
     public function created(EmployeeAttendance $employeeAttendance): void
     {
         $employeeIn = $employeeAttendance->employee_in;
         $employeeOut = $employeeAttendance->employee_out;
 
+        // employee_in ve employee_out dolu mu kontrol et
         if ($employeeIn && $employeeOut) {
+            // Giriş ve çıkış zamanlarının sırasını kontrol et
+            if (Carbon::parse($employeeOut)->lessThan(Carbon::parse($employeeIn))) {
+                // Negatif süreyi önlemek için işlem yapılmaz ve hata gösterilir
+                throw new Exception('Çıkış zamanı giriş zamanından önce olamaz!');
+            }
+
+            // Dakika cinsinden süreyi hesapla
             $duration = Carbon::parse($employeeOut)->diffInMinutes(Carbon::parse($employeeIn));
 
+            // Duration değerini dakika olarak kaydet
             $employeeAttendance->update([
                 'duration' => $duration,
             ]);
         }
     }
+
 
     /**
      * Handle the EmployeeAttendance "updated" event.
