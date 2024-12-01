314222341923cb1d4309e2ff305afef857fdabc1
qubadoff <admin@burncode.org>
2024-12-01 19:06:28 +0400

    new

--- app/Filament/Resources/VacationDayResource.php
+++ app/Filament/Resources/VacationDayResource.php
@@ -35,33 +35,6 @@ class VacationDayResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-clipboard';
 
-    protected function beforeCreate(array $data): void
-    {
-        $employeeId = $data['employee_id'];
-
-        // Employee'nin vacation day limitini al
-        $dayLimit = DB::table('employee_vacation_day_options')
-            ->where('employee_id', $employeeId)
-            ->value('day_count');
-
-        // Mevcut alınan vacation days toplamını hesapla
-        $totalVacationDays = DB::table('vacation_days')
-            ->where('employee_id', $employeeId)
-            ->sum('vacation_day_count');
-
-        // Yeni tatil günüyle toplamı kontrol et
-        if ($totalVacationDays + $data['vacation_day_count'] > $dayLimit) {
-            Notification::make()
-                ->title('Əməliyyat icra olunmadı !')
-                ->danger()
-                ->body('İşçinin məzuniyyət günlərinin sayı ' . $dayLimit . ' gündən artıq olmamalıdır !')
-                ->send();
-
-            // İşlemi durdur
-            $this->halt();
-        }
-    }
-
     public static function form(Form $form): Form
     {
         return $form
