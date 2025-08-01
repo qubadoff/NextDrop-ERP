dab54a4bb4aff5b35e67701c0ada1a9e5fab18f0
qubadoff <admin@burncode.org>
2025-08-01 18:26:27 +0400

    beta

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -38,8 +38,8 @@ class AdminPanelProvider extends PanelProvider
                 'primary' => Color::Green,
             ])
             ->maxContentWidth(MaxWidth::Full)
-            ->profile()
             ->sidebarWidth(100)
+            ->profile()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
